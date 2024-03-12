f9390678ca8a51cc1026bf0c12d0aef043b445a2
PyDOS8 <124309800+PyDOS8@users.noreply.github.com>
2024-03-11 23:48:49 -0400

    Update 2DPictureEnemy.js

--- js/2DPictureEnemy.js
+++ js/2DPictureEnemy.js
@@ -3,13 +3,50 @@ class enemy{
     constructor(x,y){
         var enemyBullets;
         var EnemyIsABossFight;
-        var AttackDamage = 1;
+        var AttackDamage;
         var enemyBulletPosition; 
+        var enemyIsShootingUp;
+        var enemyIsShootingDown;
+        var enemyIsShootingRight;
+        var enemyIsShootingLeft;
+        var enemyIsLeft;
+        var enemyIsRight;
+        var enemyIsDown;
+        var enemyIsUp;
         function EnableBossMode(){
             if(!this.EnemyIsABossFight == true){
                 this.EnemyIsABossFight = true;
-                this.AttackDamage = 10;
-                this.enemyBullets = 10;
+                this.AttackDamage = ParseInt(Math.random() * player.playerLives);
+                this.enemyBullets = ParseInt(Math.random() * 100);
+            }
+        }
+        function animateEnemy(){
+            if(this.enemyIsShootingUp == true){
+                document.getElementById("enemy").src = 'imgs/enemy/enemyShootUp.jpg';    
+            }
+            if(this.enemyIsShootingDown == true){
+                document.getElementById("enemy").src = "img/enemy/enemyShootDown.jpg";
+            }
+            if(this.enemyIsShootingLeft == true){
+                document.getElementById("enemy").src = "imgs/enemy/enemyShootLeft.jpg";
+            }
+            if(this.enemyIsShootingRight == true){
+                document.getElementById("enemy").src = "imgs/enemy/enemyShootRight.jpg";
+            }
+            if(this.enemyIsUp == true){
+                document.getElementById("enemy").src = "imgs/enemy/enemyUp.jpg";
+            }
+            if(this.enemyIsDown == true){
+                document.getElementById("enemy").src = "imgs/enemy/enemyDown.jpg";
+            }
+            if(this.enemyIsLeft == true){
+                document.getElementById("enemy").src = "imgs/enemy/enemyLeft.jpg";
+            }
+            if(this.enemyIsRight == true){
+                document.getElementById("enemy").src = "imgs/enemy/enemyRight.jpg";
+            }
+            if(this.EnemyLives == 0){
+                document.getElementById("enemy").src = "imgs/enemy/enemyDead.jpg";
             }
         }
         function DisableBossMode(){
@@ -20,104 +57,175 @@ class enemy{
             }
         }
         function ShootBulletUp(){
-            if(this.enemyBullets > 0){
-                if(player.playerLives > 0){
-                    var RandNum = ParseInt(Math.random() * 1);
-                    if(RandNum == 0){
-                        enemyBulletPoistion = document.getElementById("player").style.top;
-                        document.getElementById("enemy").style.top = ParseInt(this.enemyBulletPosition);
-                        if(player.player.playerLives > 0){
-                            player.player.playerLives = ParseInt(player.playerLives) - ParseInt(this.AttackDamage);
+            this.enemyIsShootingUp = true;
+            if(this.enemyIsShootingDown == true || this.enemyIsShootingRight == true || this.enemyIsShootingLeft){
+                this.enemyIsShootingDown = false;
+                this.enemyIsShootingUp = false;
+                this.enemyIsShootingLeft = false;
+                this.enemyIsShootingRight = false;
+            }
+            this.animateEnemy();
+                if(!player.GameOver == true){
+                    if(this.enemyBullets > 0){
+                        if(player.playerLives > 0){
+                            var RandNum = ParseInt(Math.random() * 1);
+                            if(this.RandNum == 0){
+                                this.enemyBulletPoistion = document.getElementById("player").style.top;
+                                document.getElementById("enemy").style.top = ParseInt(this.enemyBulletPosition);
+                                if(player.player.playerLives > 0){
+                                    player.player.playerLives = ParseInt(player.playerLives) - ParseInt(this.AttackDamage);
+                                }else{
+                                    player.player.GameOver();
+                                }
+                                this.enemyBullets = this.enemyBullets - 1;
+                            }else{
+                                var EnemyShipMalfunction = document.createElement("h1");
+                                this.EnemyShipMalfunction.innerText = "The enemy's ship has malfunctioned!";
+                                this.EnemyShipMalfunction.style.color = "white";
+                                document.body.appendChild(EnemyShipMalfunction);
+                            }
+                        }
+                    }else{
+                        player.GameOver();
+                    }
+                }
+            }
+            function shootBulletDown(){               
+                if(this.enemyIsShootingUp == true || this.enemyIsShootingLeft == true || this.enemyIsShootingRight == true){
+                    this.enemyIsShootingUp = false;
+                    this.enemyIsShootingLeft = false;
+                    this.enemyIsShootingRight = false;
+                }
+                this.animateEnemy();
+                if(this.enemyBullets > 0){
+                    if(!player.GameOver == true){
+                        var RandNum = ParseInt(Math.random() * 1);
+                        if(this.RandNum == 0){
+                            this.enemyBulletPosition = document.getElementById("player").style.top;
+                            document.getElementById("enemy-bullet").style.top = ParseInt(this.enemyBulletPosition);
+                            if(player.playerLives > 0){
+                                player.playerLives = ParseInt(player.playerLives) - ParseInt(this.AttackDamage);
+                            }else{
+                                player.player.GameOver();
+                            }
                         }else{
-                            player.player.GameOver();
+                            var EnemyShipMalfunction = document.createElement("h1");
+                            this.EnemyShipMalfunction.innerText = "The enemy's ship has malfunctioned!";
+                            this.EnemyShipMalfunction.style.color = "white";
+                            document.body.appendChild(EnemyShipMalfunction);
                         }
                         this.enemyBullets = this.enemyBullets - 1;
                     }else{
-                        var EnemyShipMalfunction = document.createElement("h1");
-                        EnemyShipMalfunction.innerText = "The enemy's ship has malfunctioned!";
-                        EnemyShipMalfunction.style.color = "white";
-                        document.body.appendChild(EnemyShipMalfunction);
+                        player.player.GameOver();
                     }
+                    this.enemyIsShootingDown = true;
                 }
-        }
-        function shootBulletDown(){
-            if(this.enemyBullets > 0){
-                if(player.playerLives > 0){
-                    var RandNum = ParseInt(Math.random() * 1);
-                    if(RandNum == 0){
-                        this.enemyBulletPosition = document.getElementById("player").style.top;
-                        document.getElementById("enemy-bullet").style.top = ParseInt(this.enemyBulletPosition);
-                        if(player.playerLives > 0){
-                            player.playerLives = ParseInt(player.playerLives) - ParseInt(this.AttackDamage);
-                        }else{
-                            player.player.GameOver();
-                        }
-                    }else{
-                        var EnemyShipMalfunction = document.createElement("h1");
-                        EnemyShipMalfunction.innerText = "The enemy's ship has malfunctioned!";
-                        EnemyShipMalfunction.style.color = "white";
-                        document.body.appendChild(EnemyShipMalfunction);
-                    }
-                    this.enemyBullets = this.enemyBullets - 1;
-                }else{
-                    player.player.GameOver();
-                }
-            }
         }
         function shootBulletLeft(){
-            if(this.enemyBullets > 0){
-                var RandNum = Math.Random() * 1;
-                if(player.playerLives > 0){
-                    if(RandNum == 0){
-                        this.enemyBulletPosition = document.getElementById("player").style.left;
-                        document.getElementById("enemy-bullet").style.left = ParseInt(this.enemyBulletPosition);
-                    }else{
-                        var EnemyShipMalfunction = document.createElement("h1");
-                        EnemyShipMalfunction.innerText = "The enemy's ship has malfunctioned!";
-                        EnemyShipMalfunction.style.color = "white";
-                        document.body.appendChild(EnemyShipMalfunction);
-                    }
-                    this.enemyBullets = this.enemyBullets - 1;
-                }else{
-                    player.player.GameOver();
+            if(!player.GameOver == true){
+                this.enemyIsShootingLeft = true;
+                if(this.enemyIsShootingUp == true || this.enemyIsShootingRight == true || this.enemyIsShootingDown){
+                        this.enemyIsShootingUp = false;
+                        this.enemyIsShootingDown = false;
+                        this.enemyIsShootingRight = false;                                                                                                                                                                                                                                                                               this.enemyIsShootingRight = false;
                 }
+                this.animateEnemy();
+                if(this.enemyBullets > 0){
+                    var RandNum = ParseInt(Math.random() *1);
+                    if(player.playerLives > 0){
+                        if(this.RandNum == 0){
+                            this.enemyBulletPosition = document.getElementById("player").style.left;
+                            document.getElementById("enemy-bullet").style.left = ParseInt(this.enemyBulletPosition);
+                        }else{
+                            var EnemyShipMalfunction = document.createElement("h1");
+                            this.EnemyShipMalfunction.innerText = "The enemy's ship has malfunctioned!";
+                            this.EnemyShipMalfunction.style.color = "white";
+                            document.body.appendChild(EnemyShipMalfunction);
+                        }
+                        this.enemyBullets = this.enemyBullets - 1;
+                        
+                    }else{
+                        player.GameOver();
+                    }
+                }
+            }else{
+                player.GameOver();
+            }
         }
         function shootBulletRight(){
-            if(this.enemyBullets > 0 && player.playerLives > 0){
-                var RandNum = ParseInt(Math.random() * 1);
-                if(RandNum == 0){
-                    this.enemyPosition = document.getElementById("player").style.left;
-                    document.getElementById("enemy-bullet").style.left = this.enemyPosition;
-                    player.playerLives = player.playerLives - this.AttackDamage;                       
-                }else{
-                    var EnemyShipMalfunction = document.createElement("h1");
-                    EnemyShipMalfunction.innerText = "The enemy's ship has malfunctioned!";
-                    EnemyShipMalfunction.style.color = "white";
-                    document.body.appendChild(EnemyShipMalFunction);
+            if(!player.GameOver == true){
+                if(this.enemyBullets > 0 && player.playerLives > 0){
+                    var RandNum = ParseInt(Math.random() * 1);
+                    if(this.RandNum == 0){
+                        this.enemyPosition = document.getElementById("player").style.left;
+                        document.getElementById("enemy-bullet").style.left = this.enemyPosition;
+                        player.playerLives = player.playerLives - this.AttackDamage;                       
+                    }else{
+                        var EnemyShipMalfunction = document.createElement("h1");
+                        this.EnemyShipMalfunction.innerText = "The enemy's ship has malfunctioned!";
+                        this.EnemyShipMalfunction.style.color = "white";
+                        document.body.appendChild(this.EnemyShipMalFunction);
+                    }
+                    this.enemyIsShootingRight = true;
+                    if(this.enemyIsShootingDown == true || this.enemyIsShootingLeft == true || this.enemyIsShootingUp == true){
+                        this.enemyIsShootingDown = false;
+                        this.enemyIsShootingUp = false;
+                        this.enemyIsShootingLeft = false;
+                    }
+                    this.animateEnemy();
                 }
+            }else{
+                player.GameOver();
             }
         }
         function CheckIfBulletHitEnemy(){
             var bullet = document.getElementById("player-bullet").style.position;
             var enemy = document.getElementById("enemy").style.position;
-            var EnemyLives = ParseInt(Math.random() * 10);
-            var bulletIMG = document.getElementById("player-bullet").src;
-            if(bullet == enemy){
-                if(bulletIMG == "imgs/weapons/missile.jpg"){
-                    EnemyLives = 0;
-                    player.player.GameOver();
-                    document.getElementById("enemy").src = "imgs/enemy/enemyDead.jpg";
-                }else{
-                    if(EnemyLives > 0){
-                        EnemyLives = EnemyLives - 1;
-                    }else{
+            if(!player.GameOver == true){
+                if(this.bullet == this.enemy){
+                    if(document.getElementById("player-bullet").src == "imgs/weapons/missile.jpg"){
+                        this.EnemyLives = 0;
                         player.player.GameOver();
                         document.getElementById("enemy").src = "imgs/enemy/enemyDead.jpg";
+                    }else{
+                        if(this.EnemyLives > 0){
+                            this.EnemyLives = this.EnemyLives - player.attackDamage;
+                        }else{
+                            player.GameOver();
+                            this.animateEnemy();
+                        }
                     }
+                }
+            }else{
+                player.GameOver();
+            }
+        }
+        function showPowerUp(){
+            var powerup = ParseInt(Math.random() * 1);
+            if(powerup == 0){
+                var powerupImg = document.getElementById("powerup").src;
+                powerUpImg = "imgs/powerups/extraDamage.jpg"; 
+            }
+            if(powerup == 1){
+                var powerupImg = document.getElementById("powerups").src;
+                powerupImg = "imgs/powerups/machineGun.jpg";
+            }
+            this.checkForPowerUpCollision();
+        }
+        function checkForPowerUpCollision(){
+            var playerPos = document.getElementById("player").style.position;
+            var powerUpPos = document.getElementById("powerup").style.position;
+            if(playerPos == powerUpPos){
+                var powerUp = document.getElementById("powerup").src;
+                if(powerUp == "imgs/powerups/extraDamage.jpg"){
+                    this.attackDamage = Math.random() * player.playerLives;  
+                }
+                if(powerUp == "imgs/powerups.machineGun.jpg"){
+                    var weapon = document.getElementById("enemy-bullet").src;
+                    weapon = powerUp;
+                    this.attackDamage += 5;
                 }
             }
         }
-     }
-   } 
- } 
+    }
 }
